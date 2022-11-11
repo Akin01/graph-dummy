188ca31c999d03afddbaef485d8bee000f75a707
Ainul Yaqin <Akinpasha82@gmail.com>
2022-11-11 17:27:09 +0700

    update code

--- createTrackRelation.py
+++ createTrackRelation.py
@@ -1,13 +1,4 @@
-import environ
-import os
-from .createGraph import App
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, "config.env"))
-
+from .createGraph import App, env
 
 if __name__ == "__main__":
     uri = env("NEO4J_URI")
